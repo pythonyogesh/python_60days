4cb94f60ce930aac7015affd74e9c87f33fff4de
pythonyogesh <itsyolamture@gmail.com>
2024-12-24 22:41:47 +0530

    Set

---
+++ Day1/set.py
@@ -0,0 +1,15 @@
+#pr1
+
+Set1 = {10,10,20,40,50,30}
+print(Set1)
+print(type(Set1))
+
+#pr2
+
+Set2 = {1,4,23,4,50,0}
+Set2[0]=500
+print(Set2)
+
+#Note 
+#set is immutable 
+#The set can not hold the same value or data
