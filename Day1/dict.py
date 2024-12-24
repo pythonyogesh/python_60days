588705e53f81fa2ab6f936da21e6afea6f5cdd85
pythonyogesh <itsyolamture@gmail.com>
2024-12-24 22:48:09 +0530

    dict

---
+++ Day1/dict.py
@@ -0,0 +1,13 @@
+#pr1
+
+player = {7:'Ms Dhoni',18:'Virat',45:'Rohit',1:'KL Rahul'}
+print(player)
+print(type(player))
+
+
+#pr2
+
+Menu = {1:'Tea',2:'Coffe',3:'Biryani'}
+print(Menu)
+Menu[1] = 'Burger'
+print(Menu)
