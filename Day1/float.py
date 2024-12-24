6f65e876b4b4da79ddda50b3e7dc3eb92ac38a68
pythonyogesh <itsyolamture@gmail.com>
2024-12-24 22:26:09 +0530

    float

---
+++ Day1/float.py
@@ -0,0 +1,18 @@
+#pr1
+
+Height = 5.7
+print(Height)
+print(print(type(Height)))
+
+#pr2
+
+Temperature = 23.5
+print(Temperature)
+print(type(Temperature))
+
+
+#pr3
+
+salary = 15.60
+print(salary)
+print(type(salary))
--- Day1/tuple.py
+++ Day1/tuple.py
@@ -2,3 +2,5 @@ a=(10,'sachin,,10.5')
 print(a)
 print(type(a))
 print(type(type(a)))
+
+
